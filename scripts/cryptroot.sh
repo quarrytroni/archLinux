52aa5499b8909b413a7d79c98c7cb75b1254dde9
quarrytroni <quarrytroni@gmail.com>
2026-02-23 00:09:46 +0100

    Update cryptroot.sh

--- scripts/cryptroot.sh
+++ scripts/cryptroot.sh
@@ -7,7 +7,10 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Partición de root
-ROOT_PART="/dev/nvme0n1p5"
+ROOT_PART="/dev/nvme0n1p6"
+
+# Partición de DATOS
+DATOS_PART="/dev/nvme0n1p8"
 
 # Ruta del archivo a modificar
 CRYPTTAB_FILE="/etc/crypttab.initramfs"
@@ -15,14 +18,30 @@ CRYPTTAB_FILE="/etc/crypttab.initramfs"
 # Contador de intentos
 attempts=0
 max_attempts=3
-uuid=""
+uuid_root=""
+uuid_datos=""
 
-# Verificar el UUID hasta 3 veces
+# Verificar el UUID hasta 3 veces particion root
 while [ $attempts -lt $max_attempts ]; do
-    uuid=$(blkid -s UUID -o value $ROOT_PART)
+    uuid_root=$(blkid -s UUID -o value $ROOT_PART)
     
-    if [ -n "$uuid" ]; then
-        echo "UUID encontrado: $uuid"
+    if [ -n "$uuid_root" ]; then
+        echo "UUID encontrado: $uuid_root"
+        echo "Numero de intentos: $((attempts + 1))"  
+        break
+    else
+        echo "Intento $((attempts + 1)): No se encontró UUID. Reintentando..."
+        attempts=$((attempts + 1))
+        sleep 1  # Esperar 1 segundo antes de volver a intentar
+    fi
+done
+
+# Verificar el UUID hasta 3 veces particion DATOS
+while [ $attempts -lt $max_attempts ]; do
+    uuid_datos=$(blkid -s UUID -o value $DATOS_PART)
+    
+    if [ -n "$uuid_datos" ]; then
+        echo "UUID encontrado: $uuid_datos"
         echo "Numero de intentos: $((attempts + 1))"  
         break
     else
@@ -35,7 +54,11 @@ done
 # Si se encontró un UUID
 # Verificamos que existe crypttab.initramfs y si no lo creamos y le añadimos las lineas necesarias
 # para que encripte la partición al cada inicio
-if [ -n "$uuid" ]; then
+if [ -n "$uuid_root" ]; then
+    [ ! -e $CRYPTTAB_FILE ] && cp /etc/crypttab $CRYPTTAB_FILE
+    cat >> "$CRYPTTAB_FILE" << EOF
+
+if [ -n "$uuid_datos" ]; then
     [ ! -e $CRYPTTAB_FILE ] && cp /etc/crypttab $CRYPTTAB_FILE
     cat >> "$CRYPTTAB_FILE" << EOF
 
