43093a4e4e4f7da636ca69fd431ae4ad83eb1fad
quarrytroni <quarrytroni@gmail.com>
2026-02-22 22:06:12 +0100

    Add DATOS parted

--- scripts/initmount.sh
+++ scripts/initmount.sh
@@ -4,13 +4,16 @@
 mount /dev/mapper/cryptroot /mnt && echo "cryptroot montado"
 
 mkdir /mnt/boot && echo "Directorio /boot creado"
-mount /dev/nvme0n1p4 /mnt/boot && echo "boot montado"
+mount /dev/nvme0n1p5 /mnt/boot && echo "boot montado"
 
 mkdir /mnt/boot/efi && echo "Directorio /boot/efi creado"
 mount /dev/nvme0n1p1 /mnt/boot/efi && echo "efi montado"
 
-mkdir /mnt/mnt && mkdir /mnt/mnt/WINLINUX && echo "Directorio intercambio con Windows creado"
-mount /dev/nvme0n1p6 /mnt/mnt/WINLINUX && echo "Directorio intercambio con Windows montado"
+mkdir /mnt/mnt && mkdir /mnt/mnt/DATOS && echo "Directorio /mnt/DATOS creado"
+mount /dev/mapper/cryptdatos /mnt/mnt/DATOS && echo "DATOS montado"
+
+mkdir /mnt/mnt/WINLINUX && echo "Directorio intercambio con Windows creado"
+mount /dev/nvme0n1p7 /mnt/mnt/WINLINUX && echo "Directorio intercambio con Windows montado"
 
 # mkdir /mnt/mnt/Win11 && echo "Directorio /mnt/Win11 creado"
 # mount /dev/nvme0n1p3 /mnt/mnt/Win11 && echo "Win11 montado, cuidado cualquier cambio puede dañar Windows"
