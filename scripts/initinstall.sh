aa67bc124e6f72bdb3d8909aa740140da98dbe34
quarrytroni <quarrytroni@gmail.com>
2026-02-22 22:20:46 +0100

    Update initinstall.sh

--- scripts/initinstall.sh
+++ scripts/initinstall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Instalamos el sistema base en /mnt
-pacstrap -i /mnt base base-devel fuse linux-zen linux-zen-headers linux-firmware intel-ucode usbutils nano less git btrfs-progs exfat-utils ntfs-3g grub efibootmgr networkmanager wget
+# pacstrap -i /mnt base base-devel fuse linux-zen linux-zen-headers linux-firmware intel-ucode usbutils nano less git btrfs-progs exfat-utils ntfs-3g grub efibootmgr networkmanager wget
 # exfat-utils es obsoleto, usar exfatprogs
 
 # Para AMD
-# pacstrap -i /mnt base base-devel fuse linux-zen linux-zen-headers linux-firmware amd-ucode arch-install-scripts usbutils nano less git btrfs-progs exfatprogs ntfs-3g grub efibootmgr networkmanager wget
+pacstrap -i /mnt base base-devel fuse linux-zen linux-zen-headers linux-firmware amd-ucode arch-install-scripts usbutils nano less git btrfs-progs exfatprogs ntfs-3g grub efibootmgr networkmanager wget
 
 
 # Limpiar los archivos temporales
