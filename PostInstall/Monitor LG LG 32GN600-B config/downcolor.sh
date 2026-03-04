1bc0da0bdcade0b07cfc10e5bc0622d64148d6ec
quarrytroni <quarrytroni@gmail.com>
2026-03-04 22:07:56 +0100

    del from fork c2mismo

--- PostInstall/Monitor LG LG 32GN600-B config/downcolor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ddcutil setvcp 16 80
-#sleep 0.3
-ddcutil setvcp 18 80
-#sleep 0.3
-ddcutil setvcp 1a 80
-#sleep 0.3
--- PostInstall/Monitor LG LG 32GN600-B config/upcolor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-ddcutil setvcp 16 100
-#sleep 0.3
-ddcutil setvcp 18 100
-#sleep 0.3
-ddcutil setvcp 1a 100
-#sleep 0.3
